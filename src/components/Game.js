99ea297e65efd1f30e646b1db934e3ae16353931
ezequiel cabrera <92662432+zekecabre6@users.noreply.github.com>
2025-02-07 16:31:37 -0300

    controles tactiles

--- src/components/Game.js
+++ src/components/Game.js
@@ -17,97 +17,97 @@ const Game = () => {
   const platforms = useMemo(() => {
     return [
       { position: { x: 50, y: 550 }, size: { width: 200, height: 20 } },
-    { position: { x: 300, y: 450 }, size: { width: 200, height: 20 } },
-    { position: { x: 550, y: 550 }, size: { width: 200, height: 20 } },
-    { position: { x: 800, y: 450 }, size: { width: 200, height: 20 } },
-    { position: { x: 1050, y: 550 }, size: { width: 200, height: 20 } },
-    { position: { x: 1300, y: 450 }, size: { width: 200, height: 20 } },
-    { position: { x: 1550, y: 550 }, size: { width: 200, height: 20 } },
-    { position: { x: 1800, y: 450 }, size: { width: 200, height: 20 } },
-    { position: { x: 2050, y: 550 }, size: { width: 200, height: 20 } },
-    { position: { x: 2300, y: 450 }, size: { width: 200, height: 20 } },
-    { position: { x: 2550, y: 550 }, size: { width: 200, height: 20 } },
-    { position: { x: 2800, y: 450 }, size: { width: 200, height: 20 } },
-    { position: { x: 3050, y: 550 }, size: { width: 200, height: 20 } },
-    { position: { x: 3300, y: 450 }, size: { width: 200, height: 20 } },
-    { position: { x: 3550, y: 550 }, size: { width: 200, height: 20 } },
-  ];;
+      { position: { x: 300, y: 450 }, size: { width: 200, height: 20 } },
+      { position: { x: 550, y: 550 }, size: { width: 200, height: 20 } },
+      { position: { x: 800, y: 450 }, size: { width: 200, height: 20 } },
+      { position: { x: 1050, y: 550 }, size: { width: 200, height: 20 } },
+      { position: { x: 1300, y: 450 }, size: { width: 200, height: 20 } },
+      { position: { x: 1550, y: 550 }, size: { width: 200, height: 20 } },
+      { position: { x: 1800, y: 450 }, size: { width: 200, height: 20 } },
+      { position: { x: 2050, y: 550 }, size: { width: 200, height: 20 } },
+      { position: { x: 2300, y: 450 }, size: { width: 200, height: 20 } },
+      { position: { x: 2550, y: 550 }, size: { width: 200, height: 20 } },
+      { position: { x: 2800, y: 450 }, size: { width: 200, height: 20 } },
+      { position: { x: 3050, y: 550 }, size: { width: 200, height: 20 } },
+      { position: { x: 3300, y: 450 }, size: { width: 200, height: 20 } },
+      { position: { x: 3550, y: 550 }, size: { width: 200, height: 20 } },
+    ];;
   }, []);
-  
-// Convierte las trampas en estado para poder actualizarlas
-const [traps, setTraps] = useState([
-  // { 
-  //   position: { x: 200, y: 450 }, 
-  //   size: { width: 25, height: 20 },
-  //   speed: 1,
-  //   direction: 1,
-  //   leftBound: 0, 
-  //   rightBound: 275
-  // },
-  
-  { 
-    position: { x: 675, y: 450 }, 
-    size: { width: 25, height: 20 },
-    speed: 1,
-    direction: 1,
-    leftBound: 500, 
-    rightBound: 775
-  },
-  
-  { 
-    position: { x: 1175, y: 450 }, 
-    size: { width: 25, height: 20 },
-    speed: 2,
-    direction: 1,
-    leftBound: 1000, 
-    rightBound: 1275
-  },
-  
-  { 
-    position: { x: 1675, y: 450 }, 
-    size: { width: 25, height: 20 },
-    speed: 3,
-    direction: 1,
-    leftBound: 1500, 
-    rightBound: 1775
-  },
-  
-  { 
-    position: { x: 2175, y: 450 }, 
-    size: { width: 25, height: 20 },
-    speed: 4,
-    direction: 1,
-    leftBound: 2000, 
-    rightBound: 2275
-  },
-  
-  { 
-    position: { x: 2675, y: 450 }, 
-    size: { width: 25, height: 20 },
-    speed: 5,
-    direction: 1,
-    leftBound: 2500, 
-    rightBound: 2775
-  },
-  
-  { 
-    position: { x: 3175, y: 450 }, 
-    size: { width: 25, height: 20 },
-    speed: 6,
-    direction: 1,
-    leftBound: 3000, 
-    rightBound: 3275
-  },
-   
-]);
+
+  // Convierte las trampas en estado para poder actualizarlas
+  const [traps, setTraps] = useState([
+    // { 
+    //   position: { x: 200, y: 450 }, 
+    //   size: { width: 25, height: 20 },
+    //   speed: 1,
+    //   direction: 1,
+    //   leftBound: 0, 
+    //   rightBound: 275
+    // },
+
+    {
+      position: { x: 675, y: 450 },
+      size: { width: 25, height: 20 },
+      speed: 1,
+      direction: 1,
+      leftBound: 500,
+      rightBound: 775
+    },
+
+    {
+      position: { x: 1175, y: 450 },
+      size: { width: 25, height: 20 },
+      speed: 2,
+      direction: 1,
+      leftBound: 1000,
+      rightBound: 1275
+    },
+
+    {
+      position: { x: 1675, y: 450 },
+      size: { width: 25, height: 20 },
+      speed: 3,
+      direction: 1,
+      leftBound: 1500,
+      rightBound: 1775
+    },
+
+    {
+      position: { x: 2175, y: 450 },
+      size: { width: 25, height: 20 },
+      speed: 4,
+      direction: 1,
+      leftBound: 2000,
+      rightBound: 2275
+    },
+
+    {
+      position: { x: 2675, y: 450 },
+      size: { width: 25, height: 20 },
+      speed: 5,
+      direction: 1,
+      leftBound: 2500,
+      rightBound: 2775
+    },
+
+    {
+      position: { x: 3175, y: 450 },
+      size: { width: 25, height: 20 },
+      speed: 6,
+      direction: 1,
+      leftBound: 3000,
+      rightBound: 3275
+    },
+
+  ]);
 
 
   //calcula la posicion luego de la ultima plataforma
   const lastPlatform = platforms[platforms.length - 1];
-  
+
   const goalPosition = lastPlatform.position.x + lastPlatform.size.width - 200;
 
-//
+  //
 
   //reinicia juego
   const restartGame = () => {
@@ -134,13 +134,36 @@ const [traps, setTraps] = useState([
         setVelocity((prev) => ({ ...prev, x: 0 }));
       }
     };
+    // Agregar eventos táctiles (móviles)
+    const handleTouchStart = (event) => {
+      // Detectar la zona del toque
+      const touchX = event.touches[0].clientX;
+      const screenWidth = window.innerWidth;
+
+      // Mover jugador a la derecha o izquierda
+      if (touchX < screenWidth / 2) {
+        setVelocity((prev) => ({ ...prev, x: -5 })); // Mover izquierda
+      } else {
+        setVelocity((prev) => ({ ...prev, x: 5 })); // Mover derecha
+      }
+    };
+
+    const handleTouchEnd = () => {
+      setVelocity((prev) => ({ ...prev, x: 0 })); // Detener movimiento al soltar
+    };
+
+    // Asignar los eventos táctiles
+    window.addEventListener('touchstart', handleTouchStart);
+    window.addEventListener('touchend', handleTouchEnd);
 
     window.addEventListener('keydown', handleKeyDown);
     window.addEventListener('keyup', handleKeyUp);
 
     return () => {
-      window.removeEventListener('keydown', handleKeyDown);
-      window.removeEventListener('keyup', handleKeyUp);
+      window.removeEventListener('touchstart', handleTouchStart);
+    window.removeEventListener('touchend', handleTouchEnd);
+    window.removeEventListener('keydown', handleKeyDown);
+    window.removeEventListener('keyup', handleKeyUp);
     };
   }, [isJumping, gameOver]);
 
@@ -172,8 +195,8 @@ const [traps, setTraps] = useState([
         // Detección de colisiones con el suelo
         if (newY >= 550) {
           setGameOver(true);
-            alert('¡Has caído en una trampa! Inténtalo de nuevo');
-            restartGame();
+          alert('¡Has caído en una trampa! Inténtalo de nuevo');
+          restartGame();
         }
 
         // Detección de colisiones con plataformas
